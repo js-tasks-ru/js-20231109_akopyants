c2a88dfd5115172a9b6071ca0cf148d8dc6253f3
Akopyants <akopyants92@yandex.ru>
2023-11-14 11:29:00 +0300

    upd

--- 02-javascript-data-types/1-sort-strings/index.js
+++ 02-javascript-data-types/1-sort-strings/index.js
@@ -7,7 +7,7 @@
 export function sortStrings(arr, param = "asc") {
   const sortType = param === "asc" ? 1 : -1;
   const sorting = (a, b) =>
-    sortType * a.localeCompare(b, undefined, { caseFirst: "upper" });
+    sortType * a.localeCompare(b, [], { caseFirst: "upper" });
 
   return [...arr].sort(sorting);
 }
