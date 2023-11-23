ee0a3040201241d9ed95c09df986d1da80512ea5
Akopyants <akopyants92@yandex.ru>
2023-11-24 00:38:46 +0300

    NotificationMessage компонент

--- 05-dom-document-loading/1-notification/index.js
+++ 05-dom-document-loading/1-notification/index.js
@@ -1,3 +1,57 @@
 export default class NotificationMessage {
+  static currentNotification = null;
 
+  constructor(
+    notificationText = "",
+    { duration = "", type = "", target = "" } = {}
+  ) {
+    this.notificationText = notificationText;
+    this.duration = duration;
+    this.type = type;
+    this.target = target;
+    this.element = this.createElement(this.createTemplate());
+  }
+
+  createElement(html) {
+    const div = document.createElement("div");
+    div.innerHTML = html;
+    return div.firstElementChild;
+  }
+
+  createTemplate() {
+    return `<div class="notification ${this.type}" style="--value: ${this.duration}ms">
+                <div class="timer"></div>
+                <div class="inner-wrapper">
+                <div class="notification-header">${this.type}</div>
+                <div class="notification-body">
+                    ${this.notificationText}
+                </div>
+                </div>
+            </div>`;
+  }
+
+  show(target = document.body) {
+    if (NotificationMessage.currentNotification) {
+      NotificationMessage.currentNotification.remove();
+    }
+
+    target.append(this.element);
+    NotificationMessage.currentNotification = this;
+
+    setTimeout(() => {
+      this.remove();
+    }, this.duration);
+  }
+
+  remove() {
+    this.element.remove();
+
+    if (NotificationMessage.currentNotification === this) {
+      NotificationMessage.currentNotification = null;
+    }
+  }
+
+  destroy() {
+    this.remove();
+  }
 }
